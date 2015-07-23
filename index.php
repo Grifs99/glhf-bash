08094f98e90bf1e6c842231799aa850a0ec14498
Martin <olekss.martins@gmail.com>
2015-07-23 20:19:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             <?php endif ?>
             </div>
         <?php endforeach ?>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </body>
 </html>
 
