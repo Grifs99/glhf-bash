0ef741bac65af1332fffa2a883ba1539c6cb4de7
Martin <olekss.martins@gmail.com>
2015-07-23 20:18:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <title>GLHF.lv Teamspeak Bash</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </head>
     <body>
